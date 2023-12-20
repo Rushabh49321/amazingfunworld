8a596480a2c32df580eaa8fc1d72aab511293640
gohelrushabh <rushabh.micrasol@gmail.com>
2023-12-20 15:43:23 +0530

    first commit

---
+++ assests/js/script.js
@@ -0,0 +1,88 @@
+// Define image data for each theme
+const themeData = {
+    'Theme Park': [
+        { title: 'KRISHNA ART MUSEUM', imageUrl: './assests/images/Theme1.jpg' },
+        { title: '5D THEATRE', imageUrl: './assests/images/Theme2.jpg' },
+        { title: '3D SELFIE ZONE', imageUrl: './assests/images/Theme3.jpg' },
+        { title: 'MIRROR MAZE', imageUrl: './assests/images/Theme4.jpg' },
+        { title: 'HORROR THEME', imageUrl: './assests/images/Theme5.jpg' },
+        { title: 'JUNGLE THEME', imageUrl: './assests/images/Theme6.jpg' },
+        { title: 'KRISHNA GATHA', imageUrl: './assests/images/Theme7.jpg' },
+    ],
+    'Amusement Park': [
+        { title: 'OCTOPUS', imageUrl: './assests/images/amusement1.jpg' },
+        { title: 'BIG PENDULUM', imageUrl: './assests/images/amusement2.jpg' },
+        { title: 'REPEAL FARE', imageUrl: './assests/images/amusement3.jpg' },
+        { title: 'TOP SPRING', imageUrl: './assests/images/amusement4.jpg' },
+        { title: 'FLYING CHAIR', imageUrl: './assests/images/amusement5.jpg' },
+        { title: 'SURFER RIDE', imageUrl: './assests/images/amusement6.jpg' },
+        { title: 'DRESSING CAR', imageUrl: './assests/images/amusement7.jpg' },
+        { title: 'BOUNCY', imageUrl: './assests/images/amusement8.jpg' },
+        { title: 'TRAMPOLINE - FIRST IN GUJARAT', imageUrl: './assests/images/amusement9.jpg' },
+    ],
+    'Resort': [
+        { imageUrl: './assests/images/resort1.jpg' },
+        { imageUrl: './assests/images/resort2.jpg' },
+        { imageUrl: './assests/images/resort3.jpg' },
+        { imageUrl: './assests/images/resort4.jpg' },
+        { imageUrl: './assests/images/resort5.jpg' },
+        { imageUrl: './assests/images/resort6.jpg' },
+        { imageUrl: './assests/images/resort7.jpg' },
+        { imageUrl: './assests/images/resort8.jpg' },
+    ],
+};
+
+function openPopup(theme) {
+    // Get the container for title and images
+    const popupTitleContainer = document.getElementById("popupTitle");
+    const popupImagesContainer = document.getElementById("popupImages");
+
+    // Remove existing title and images
+    popupTitleContainer.innerHTML = '';
+    popupImagesContainer.innerHTML = '';
+
+    // Set the title for the popup
+    const popupTitle = document.createElement('h2');
+    popupTitle.textContent = theme;
+    popupTitleContainer.appendChild(popupTitle);
+
+    // Create a grid container
+    const gridContainer = document.createElement('div');
+    gridContainer.className = 'image-grid';
+
+    // Add images for the selected theme
+    themeData[theme].forEach(data => {
+        const imgElement = document.createElement('img');
+        imgElement.src = data.imageUrl;
+        imgElement.alt = data.title; // Set alt text as the title
+
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        const caption = document.createElement('p');
+        caption.textContent = data.title;
+        gridItem.appendChild(imgElement);
+        gridItem.appendChild(caption);
+        gridContainer.appendChild(gridItem);
+    });
+
+
+    popupImagesContainer.appendChild(gridContainer);
+
+    document.getElementById("popupContainer").style.display = "flex";
+}
+
+function closePopup() {
+    document.getElementById("popupContainer").style.display = "none";
+}
+function sendMessage() {
+    
+    const phoneNumber = '+919427444848';
+    const message = 'Hello! I would like to avail the 10% discount.';
+
+    // Construct the WhatsApp API endpoint
+    const apiEndpoint = ` https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+   
+
+    // Open the WhatsApp API endpoint in a new tab
+    window.open(apiEndpoint, '_blank');
+}
